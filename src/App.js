a45995c12c3eddd203eb29a050f1fe89e3c59767
Muskan Saini <muskansaini@Muskans-MacBook-Air.local>
2024-07-20 14:38:22 +0530

    resolve the todo and fix the form

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPinterest } from "@fortawesome/free-brands-svg-icons";
-import { faSave } from "@fortawesome/free-solid-svg-icons";
+// import { useState } from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPinterest } from "@fortawesome/free-brands-svg-icons";
+// import { faSave } from "@fortawesome/free-solid-svg-icons";
 
 import "./App.css";
 
 function App() {
-  const [hover, setHover] = useState(false);
-  const [hoverBtn, setHoverBtn] = useState(false);
+  // const [hover, setHover] = useState(false);
+  // const [hoverBtn, setHoverBtn] = useState(false);
 
-  const openNewWindowHandler = () => {
-    const width = 800;
-    const height = 600;
-    const left = window.screen.width / 2 - width / 2;
-    const top = window.screen.height / 2 - height / 2;
+  // const openNewWindowHandler = () => {
+  //   const width = 800;
+  //   const height = 600;
+  //   const left = window.screen.width / 2 - width / 2;
+  //   const top = window.screen.height / 2 - height / 2;
 
-    // Open a new window with initial dimensions
-    const newWindow = window.open(
-      "https://in.pinterest.com/pin-builder/?description=Indistractable+traction+distraction+triggers+diagram&is_video=false&media=https%3A%2F%2Fwww.nirandfar.com%2Fwp-content%2Fuploads%2F2019%2F05%2FIndistractable-traction-distraction-triggers-diagram.jpg&method=bookmarklet&url=https%3A%2F%2Fwww.nirandfar.com%2Findistractable-tools",
-      "popup",
-      `width=${width},height=${height},top=${top},left=${left},resizable,scrollbars,status`
-    );
+  //   // Open a new window with initial dimensions
+  //   const newWindow = window.open(
+  //     "https://in.pinterest.com/pin-builder/?description=Indistractable+traction+distraction+triggers+diagram&is_video=false&media=https%3A%2F%2Fwww.nirandfar.com%2Fwp-content%2Fuploads%2F2019%2F05%2FIndistractable-traction-distraction-triggers-diagram.jpg&method=bookmarklet&url=https%3A%2F%2Fwww.nirandfar.com%2Findistractable-tools",
+  //     "popup",
+  //     `width=${width},height=${height},top=${top},left=${left},resizable,scrollbars,status`
+  //   );
 
-    if (newWindow) {
-      newWindow.focus();
-    }
-  };
+  //   if (newWindow) {
+  //     newWindow.focus();
+  //   }
+  // };
 
   return (
     <>
-      <div
+      {/* <div
         style={{ position: "relative", width: "800px" }}
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
@@ -74,7 +74,7 @@ function App() {
         ) : (
           ""
         )}
-      </div>
+      </div> */}
       <form name="contact" method="POST" data-netlify="true">
   <p>
     <label>Your Name: <input type="text" name="name" /></label>
